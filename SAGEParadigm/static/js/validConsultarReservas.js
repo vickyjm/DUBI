370184a9fe6a8ad086b6e42c46db2e0420156787
vickyjm <mvjorgemauriello@gmail.com>
2015-03-01 10:08:54 -0430

    Agregué verificación dinámica de la cédula.

---
+++ SAGEParadigm/static/js/validConsultarReservas.js
@@ -0,0 +1,23 @@
+$(function(){
+	$('[data-toggle="popover"]').popover({
+        trigger: 'manual',
+        'placement': 'right',
+        container: 'body',
+        'show': true
+    });
+	
+	$('#id_cedula').keyup(function(){
+		var _this = $('#id_cedula');
+		var _valor = $('#id_cedula').val();
+		var regEx = /^([1-9][0-9]{0,3})(\.?[0-9]{3}){0,2}$/;
+		var valid = regEx.test(_valor);
+		
+		if (valid){
+			_this.popover('hide');
+		} else {
+			_this.popover('show');
+		}
+		
+	});
+	
+});
